80199844e1bf26a986f83b58a692ba84a4a593a1
Hy-An Exequiel <nathan.hyan2048@gmail.com>
2020-10-19 23:53:28 -0300

    Removed wowjs

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles/main.scss";
-import WOW from "wowjs";
-new WOW.WOW({ mobile: false }).init();
 
 ReactDOM.render(
   <React.StrictMode>
